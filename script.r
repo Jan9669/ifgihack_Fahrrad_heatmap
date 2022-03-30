93e46c1d37a235154c59bab4739d32cd2b34c0af
Gustav <61976211+OTI2020@users.noreply.github.com>
2022-03-30 18:42:59 +0200

    create script.r
    this script is for manupulating csv with counts of bikes

---
+++ script.r
@@ -0,0 +1,5 @@
+
+# read csv
+leezen_counts <- read.csv("C:\\Users\\49157\\Documents\\FS_6_SoSe_22\\ifgiHack\\radverkehr-zaehlstellen-main\\radverkehr-zaehlstellen-main\\100031297\\2022-03.csv")
+head(leezen_counts)
+class(leezen_counts)
